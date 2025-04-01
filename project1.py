261874377af7a74c644aaf2b67a0eefa922950b2
Angel Marcel <angelmarcel364@gmail.com>
2025-04-01 11:40:59 +0300

    function sinal appearence

--- project1.py
+++ project1.py
@@ -1,5 +1,5 @@
 from tkinter import *
-#def Sample1(win):
+def Sample1(win):
     def sinal(t1: Toplevel, name, age, music, drinks, food):
 
         Text = f'''
@@ -7,4 +7,8 @@ from tkinter import *
            when i'm listerning music i prefer to use {drinks} and some bytes and my 
            and my favourite food is {food}'''
         
-        
+        t1.geometry(newGeometry=('500x500'))
+        Label(t1, text=('Story:'), wraplength=t1.winfo_width()).place(x=160,y=310)
+        Label(t1, text=Text, wraplength=t1.winfo_width()).place(x=0,y=330) 
+
+    
