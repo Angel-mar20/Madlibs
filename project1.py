b293536d706fbae26651ab0c056538626a62bad2
Angel Marcel <angelmarcel364@gmail.com>
2025-04-01 11:38:44 +0300

    function sinal text

---
+++ project1.py
@@ -0,0 +1,10 @@
+from tkinter import *
+#def Sample1(win):
+    def sinal(t1: Toplevel, name, age, music, drinks, food):
+
+        Text = f'''
+           my name is {name}, i have {age} years old my favourite music is {music}
+           when i'm listerning music i prefer to use {drinks} and some bytes and my 
+           and my favourite food is {food}'''
+        
+        
