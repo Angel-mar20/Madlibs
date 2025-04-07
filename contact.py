aee9f8297b603480af62423c1670be98eb213da1
Angel Marcel <angelmarcel364@gmail.com>
2025-04-07 17:57:41 +0300

    function update2

--- contact.py
+++ contact.py
@@ -72,4 +72,12 @@ def UpdateDetail():
 	elif not(Name.get()) and not(Number.get()) and not(len(select.curselection())==0):
 		messagebox.showerror("Error", "Please fill the information")
 
-	
+	else:
+		if len(select.curselection())==0:
+			messagebox.showerror("Error", "Please Select the Name and \n press Load button")
+		else:
+			message1 = """To Load the all information of \n 
+						  selected row press Load button\n.
+						  """   
+			messagebox.showerror("Error", message1)
+
