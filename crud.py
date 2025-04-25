f901a92a7d45f3304b3320e9ea22844c8ffd6a2a
Angel Marcel <angelmarcel364@gmail.com>
2025-04-25 16:07:16 +0300

    class function init

--- crud.py
+++ crud.py
@@ -1,3 +1,10 @@
 import json
 import os
 
+class Task:
+    def __init__(self, title, description):
+        self.title = title
+        self.description = description
+        self.completed = False
+
+    
